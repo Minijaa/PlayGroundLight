1b6111fd0029c0067122c4815722796e353ac27b
Minijaa <saho6409@student.su.se>
2018-04-24 14:21:01 +0200

    test

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
