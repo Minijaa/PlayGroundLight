28f6083b3ce17fe09b2c8929dffe24ce750b7083
adamblo <adamablomqvist@gmail.com>
2018-04-25 00:06:57 +0200

    Test
    Vad kan gå fel?

--- src/main/java/com/group05/playground/Greeting.java
+++ src/main/java/com/group05/playground/Greeting.java
@@ -1,4 +1,15 @@
+package com.group05.playground;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+
 public class Greeting {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String data;
 
     public Greeting(String data){
--- src/main/java/com/group05/playground/HelloController.java
+++ src/main/java/com/group05/playground/HelloController.java
@@ -1,3 +1,5 @@
+package com.group05.playground;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
