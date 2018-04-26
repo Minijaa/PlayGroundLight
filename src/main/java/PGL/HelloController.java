05f85409be8fd3426eb1ba2179053aea58ccc3f9
Minijaa <saho6409@student.su.se>
2018-04-26 13:20:38 +0200

    endpoint: list

--- src/main/java/PGL/HelloController.java
+++ src/main/java/PGL/HelloController.java
@@ -1,12 +1,15 @@
 package PGL;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.lang.reflect.Array;
+
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8100","file://"})
+@CrossOrigin(origins = {"http://localhost:8100", "file://"})
 public class HelloController {
     private static final String template = "Hello, %s!";
 
@@ -15,4 +18,8 @@ public class HelloController {
         return new Greeting(String.format(template, name));
     }
 
+    @RequestMapping("/list")
+    public Greeting[] getList() {
+        return new Greeting[]{new Greeting("Pelle"), new Greeting("Olle"), new Greeting("Nisse")};
+    }
 }
