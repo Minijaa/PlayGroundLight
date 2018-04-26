6d5daa392ab4720bc7d5893e0f8f128d8ec9f61d
ASchub <ASchub@users.noreply.github.com>
2018-04-26 13:08:26 +0200

    Working API-ish

--- src/main/java/PGL/HelloController.java
+++ src/main/java/PGL/HelloController.java
@@ -1,10 +1,12 @@
 package PGL;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:8100","file://"})
 public class HelloController {
     private static final String template = "Hello, %s!";
 
