98f50da2bac67aa6de440e9021b9ba694cdda114
Minijaa <saho6409@student.su.se>
2018-04-30 11:08:28 +0200

    Lyktstolpe implementerad

---
+++ src/main/java/PGL/LightPost.java
@@ -0,0 +1,27 @@
+package PGL;
+
+public class LightPost {
+    private String name;
+    private int color;
+
+    public LightPost(String name, int color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
