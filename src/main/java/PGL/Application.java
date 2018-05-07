794abc5360ea17100f1e0d90c76b4afa5461e4f0
Tor-Olsson <tor.olsson@gmail.com>
2018-05-07 11:01:19 +0200

    removed bean-test

--- src/main/java/PGL/Application.java
+++ src/main/java/PGL/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -15,7 +14,5 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
-
-
     }
 }
