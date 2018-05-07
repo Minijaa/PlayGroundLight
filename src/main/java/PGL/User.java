2c0895b35fb2b56871dc2767d7d74e31c3ca24e8
Tor-Olsson <tor.olsson@gmail.com>
2018-05-07 10:43:43 +0200

    uncommented imports

--- src/main/java/PGL/User.java
+++ src/main/java/PGL/User.java
@@ -1,16 +1,16 @@
 package PGL;
-/*
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;*/
+import javax.persistence.Id;
 import java.util.ArrayList;
 
-//@Entity
+@Entity
 public class User {
 
-  //  @Id
-   // @GeneratedValue(strategy=GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
     private String name;
     private String email;
